2cc0d12e5f31322eb7cdf89273be064df53f4f5e
op48 <lianne.tan@gmail.com>
2014-07-01 17:02:02 +0100

    Added a user signup feature

---
+++ features/sign_up.feature
@@ -0,0 +1,11 @@
+Feature: Sign up user
+
+Background: 
+    Given that a user is on the signup page
+
+@wip
+Scenario:
+  When a user completes the form with valid information
+    And chooses "Bride" as their role
+  Then a "Bride" account should be created
+  
---
+++ features/step_definitions/signup_steps.rb
@@ -0,0 +1,15 @@
+Given(/^that a user is on the signup page$/) do
+  
+end
+
+When(/^a user completes the form with valid information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^chooses "(.*?)" as their role$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a "(.*?)" account should be created$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
