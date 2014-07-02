925e0e75599f82f158a49d14f2a9aabe7c1c4c20
op48 <lianne.tan@gmail.com>
2014-07-02 18:02:01 +0100

    Dropped brides table

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701161410) do
+ActiveRecord::Schema.define(version: 20140702165912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "brides", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
