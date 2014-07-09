91d212411da8ae3e50719ece2b94345781ba4e3e
op48 <lianne.tan@gmail.com>
2014-07-09 18:28:23 +0100

    Added model tasks with table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708160642) do
+ActiveRecord::Schema.define(version: 20140709165242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140708160642) do
     t.integer  "user_id"
     t.string   "title"
     t.string   "role_description"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
