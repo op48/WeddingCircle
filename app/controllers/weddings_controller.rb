ddc448f8edb73213edbfe4c65997f3e1166c71f5
op48 <lianne.tan@gmail.com>
2014-07-09 11:08:48 +0100

    Added nested attributes, using new and create action with current user

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,22 +1,40 @@
 class WeddingsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:authorise_facebook, :facebook_oauth_callback] 
+  
+  def new
+    @user = current_user
+    @wedding = Wedding.new
+    @wedding.roles.build  
+  end
+
+  def create
+    #binding.pry
+    @user = current_user
+    @wedding = Wedding.new(allowed_params)
+    @wedding.save!
+    redirect_to wedding_path(@wedding)
+  end
 
   def show
     @wedding = Wedding.find(params[:id])
+    #@role = Role.find(params[:id])
   end
 
   def edit
     @wedding = Wedding.find(params[:id])
+    #binding.pry
   end
 
   def update
     @wedding = Wedding.find(params[:id])
+    #binding.pry
     @wedding.update(allowed_params)
     redirect_to wedding_path
   end
 
   private
   def allowed_params
-    params.require(:wedding).permit(:title, :wedding_date)
+    params.require(:wedding).permit(:id, :title, :wedding_date, {roles_attributes: [:id, :title, :user_id, :role_description]})
   end
 
 end
