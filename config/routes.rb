cc9a9df41a5d919fe7457bc5eb0d1df90130b5dc
op48 <lianne.tan@gmail.com>
2014-07-09 18:27:51 +0100

    Added routing to added actions in created in relevant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
-
-  resources :users do
+  
+  resources :weddings, :only => [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :roles, :only => [:index, :create, :destroy]
+  end
+ 
+  # resource :user, :only => [] do
+  #   member do
+  #     get :me
+  #   end
+  # end
+ 
+  resources :users, :only => [] do
     collection do
       get :authorise_facebook
-      #get :github_oauth_callback
     end
   end
 
+  resource :user, :only => [:show] 
+  
   get 'facebook_oauth_callback', { #create verb
     :to => 'users#facebook_oauth_callback', #create action
     :as => :facebook_oauth_callback #create path helper
