4932c508fd0e4e7468d291a5d79892b6498fa845
op48 <lianne.tan@gmail.com>
2014-07-09 11:14:25 +0100

    Added description to roles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708150318) do
+ActiveRecord::Schema.define(version: 20140708160642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140708150318) do
     t.integer  "wedding_id"
     t.integer  "user_id"
     t.string   "title"
+    t.string   "role_description"
   end
 
   create_table "users", force: true do |t|
