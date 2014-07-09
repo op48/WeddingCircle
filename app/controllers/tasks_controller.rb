b7d646426012986bf783474eaa80e2109507a387
op48 <lianne.tan@gmail.com>
2014-07-09 21:10:04 +0100

    Added crud actions for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  before_filter :authenticate_user!, :except => [:authorise_facebook, :facebook_oauth_callback] 
+
+  def new
+    @user = current_user
+    @task = Task.new
+  end 
+
+  def create
+    @user = current_user
+    @task = Task.new(allowed_params)
+    @task.save!
+    redirect_to tasks_path
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def edit
+    @task = Task.find(params[:id]) 
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(allowed_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def allowed_params
+    params.require(:task).permit(:wedding_id, :title, :task_details, :date_time_due, :location)
+  end
+
 end
