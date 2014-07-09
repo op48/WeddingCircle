3fd00b4ea85fe92ed8c2c48f34eacfd09edf567f
op48 <lianne.tan@gmail.com>
2014-07-09 18:41:36 +0100

    Set up the associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  belongs_to :wedding
+  has_many :roles, :through => :weddings
 end
--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,6 +1,9 @@
 class Wedding < ActiveRecord::Base
   has_many :roles
   has_many :users, :through => :roles
+  has_many :tasks
 
   accepts_nested_attributes_for :roles
 end
+
+
