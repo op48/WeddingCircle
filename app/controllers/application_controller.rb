9cdc5541b9f3e2da74e4d9eca686c8a768a201c0
op48 <lianne.tan@gmail.com>
2014-07-14 14:00:16 +0100

    Added a before filter to authenticate user, with a method for after sign in with current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_user!, :except => [:authorise_facebook, :facebook_oauth_callback]
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user) #your path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+  end
+
 end
