8a97cf701f128a137fb5e7b9eb2a94f0f2d380aa
op48 <lianne.tan@gmail.com>
2014-07-15 14:05:49 +0100

    Removed unused association

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,6 +3,4 @@ require 'rails_helper'
 RSpec.describe Role, :type => :model do
   it { should belong_to(:user) }
   it { should belong_to(:wedding) }
-  it { should have_many(:tasks).through(:weddings)}
-
 end
